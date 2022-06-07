f602f3a4f615a2a69bd113f5038c7089576dcce9
Evgeniy Kartsev <evgeniy.kartsev@evocargo.com>
2022-06-07 18:44:48 +0300

    ADD: homework4: add adaptive alignment

--- homework4/include/frame_matching.h
+++ homework4/include/frame_matching.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <tuple>
 
 #include "opencv2/opencv.hpp"
 #include "opencv2/features2d.hpp"
@@ -13,9 +14,11 @@ inline bool break_for_key(int delay) {
         case 27:
             return true;
         case 32:
-            while(true) {
-                if(cv::waitKey(delay) == 32) {
-                    break;
+            while (true) {
+                if (cv::waitKey(delay) == 32) {
+                    return false;
+                } else if (cv::waitKey(delay) == 27) {
+                    return true;
                 }
             }
         default:
@@ -23,9 +26,37 @@ inline bool break_for_key(int delay) {
     }
 }
 
+inline std::tuple<cv::Mat, cv::Mat, std::vector<cv::KeyPoint>>
+grab_on_image(cv::VideoCapture &capture,
+              cv::Ptr<cv::FeatureDetector> &detector,
+              cv::Ptr<cv::DescriptorExtractor> &extractor,
+              bool using_adaptive_alignment = false) {
+    cv::Mat src;
+    cv::Mat deser;
+    std::vector<cv::KeyPoint> keys;
+
+    capture.read(src);
+
+    if(!src.empty()) {
+        if (using_adaptive_alignment) {
+            cv::cvtColor(src, src, CV_RGB2GRAY);
+
+            cv::Ptr<cv::CLAHE> pClahe = createCLAHE(80, cv::Size(5, 5));
+            pClahe->setClipLimit(1);
+
+            pClahe->apply(src, src);
+            cv::cvtColor(src, src, CV_GRAY2RGB);
+        }
+
+        detector->detect(src, keys);
+        extractor->compute(src, keys, deser);
+    }
+
+    return std::make_tuple(src, deser, keys);
+};
+
 template<class Detector>
-void frame_matching(const std::string& path) {
-    using namespace cv;
+void frame_matching(const std::string &path, bool using_adaptive_alignment = false) {
     cv::Ptr<cv::FeatureDetector> detector = Detector::create();
     cv::Ptr<cv::DescriptorExtractor> extractor = Detector::create();
     cv::BFMatcher matcher;
@@ -35,46 +66,46 @@ void frame_matching(const std::string& path) {
 
     cv::Mat src;
     cv::Mat prev_src;
-    cv::Mat prev_deser;
+
     cv::Mat deser;
+    cv::Mat prev_deser;
+
     cv::Mat image_with_points;
 
     cv::VideoCapture cap(path);
-    if(!cap.isOpened() || !cap.grab()) {
+    if (!cap.isOpened()) {
         throw std::invalid_argument("incorrect path: " + path);
     }
 
     int delay = static_cast<int>(1000 / cap.get(cv::CAP_PROP_FPS));
 
-    cap.read(prev_src);
-    detector->detect(prev_src, prev_keys);
-    extractor->compute(prev_src, prev_keys, prev_deser);
+    std::tie(prev_src, prev_deser, prev_keys) = grab_on_image(cap, detector, extractor, using_adaptive_alignment);
 
-    while(true) {
+    while (true) {
         std::vector<cv::DMatch> matches;
 
-        cap.read(src);
+        std::tie(src, deser, keys) = grab_on_image(cap, detector, extractor, using_adaptive_alignment);
 
-        if(src.empty()) {
+        if (src.empty()) {
             break;
         }
-
-        detector->detect(src, keys);
-        extractor->compute(src, keys, deser);
 
         matcher.match(deser, prev_deser, matches);
         cv::drawMatches(src, keys, prev_src, prev_keys, matches, image_with_points);
 
         cv::imshow(typeid(Detector).name(), image_with_points);
+        cv::imshow("Origin", src);
 
         prev_src = std::move(src);
         prev_keys = std::move(keys);
         prev_deser = std::move(deser);
 
-        if(break_for_key(delay)) {
+        if (break_for_key(delay)) {
             break;
         }
     }
+
     cap.release();
     cvDestroyWindow(typeid(Detector).name());
+    cvDestroyWindow("Origin");
 }
